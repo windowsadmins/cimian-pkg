a1b616b3def48e878fc48dd789edbd61cbb502e2
Rod Christiansen <rod@focused.systems>
2025-06-27 11:55:15 -0700

    Dropping `Substring()` from InstallScript build

--- main.go
+++ main.go
@@ -297,7 +297,7 @@ $payloadRoot = Join-Path $PSScriptRoot '..\payload'
 Get-ChildItem -Path $payloadRoot -Recurse | ForEach-Object {
     $fullName = $_.FullName
     $fullName = [Management.Automation.WildcardPattern]::Escape($fullName)
-    $relative = $fullName.Substring($payloadRoot.Length).TrimStart('\','/')
+    $relative = $fullName.Replace($payloadRoot, '').TrimStart('\','/')
     $dest     = Join-Path $installLocation $relative
 
     if ($_.PSIsContainer) {
